e92da5575377c9ab5ed24399c73469c488b98096
douqin <dagxuanlam@gmail.com>
2024-02-27 15:28:22 +0700

    update env for redis

--- server-app-chat-chit/lib/database/redis/redis.ts
+++ server-app-chat-chit/lib/database/redis/redis.ts
@@ -1,16 +1,16 @@
 import { Redis } from "ioredis";
 
 const REDIS_HOST = process.env.REDIS_HOST || "";
-const REDIS_PORT = process.env.REDIS_PORT || "";
+const REDIS_PORT = Number(process.env.REDIS_PORT);
 export class DatabaseCache {
   private static redis: Redis;
 
-  public constructor() {}
+  public constructor() { }
 
   public getInstance(): Redis {
-    if (DatabaseCache.redis) return DatabaseCache.redis;
-    return DatabaseCache.redis = new Redis({
-      maxRetriesPerRequest : null
+    return DatabaseCache.redis = new Redis(
+      REDIS_PORT, REDIS_HOST, {
+      maxRetriesPerRequest: null,
     });
   }
 }
